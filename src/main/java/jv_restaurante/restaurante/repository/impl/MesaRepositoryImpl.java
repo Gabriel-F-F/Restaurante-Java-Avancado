a7fc136c0f3c02ac639026ce11e2fdb0757bdb31
Gabriel-F-F <gfelixfaustina@gmail.com>
2024-09-27 17:29:45 -0300

    Commit: WebConfig

--- src/main/java/jv_restaurante/restaurante/repository/impl/MesaRepositoryImpl.java
+++ src/main/java/jv_restaurante/restaurante/repository/impl/MesaRepositoryImpl.java
@@ -13,26 +13,32 @@ import jv_restaurante.restaurante.repository.MesaRepositoryCustom;
 
 public class MesaRepositoryImpl implements MesaRepositoryCustom {
 
-	@PersistenceContext
-	private EntityManager em;
-	
-	final QRestauranteEntity restaurante = QRestauranteEntity.restauranteEntity;
-	final QReservaEntity reserva = QReservaEntity.reservaEntity;
-	final QMesaEntity mesa = QMesaEntity.mesaEntity;
-	
-	public List<MesaEntity> getMesasPorCapacidadePessoa(Long restauranteId, Integer capacidadePessoas, LocalDate data) {
-		var query = new JPAQuery<MesaEntity>(em);
-		
-		query.select(mesa)
-			.distinct()
-			.from(restaurante)
-			.innerJoin(restaurante.mesas, mesa)
-			.innerJoin(mesa.reservas, reserva)
-			.where(restaurante.id.eq(restauranteId)
-			.and(mesa.capacidadePessoas.eq(capacidadePessoas) //terminou aq
-			.and(reserva.dataReserva.eq(data))
-			.and(reserva.status.eq())));
-		
-		return query.fetch();
+	@Override
+	public List<MesaEntity> getMesasPorCapacidadePessoa(Long idRestaurante, Integer capacidadePessoas, LocalDate data) {
+		// TODO Auto-generated method stub
+		return null;
 	}
+//
+//	@PersistenceContext
+//	private EntityManager em;
+//	
+//	final QRestauranteEntity restaurante = QRestauranteEntity.restauranteEntity;
+//	final QReservaEntity reserva = QReservaEntity.reservaEntity;
+//	final QMesaEntity mesa = QMesaEntity.mesaEntity;
+//	
+//	public List<MesaEntity> getMesasPorCapacidadePessoa(Long restauranteId, Integer capacidadePessoas, LocalDate data) {
+//		var query = new JPAQuery<MesaEntity>(em);
+//		
+//		query.select(mesa)
+//			.distinct()
+//			.from(restaurante)
+//			.innerJoin(restaurante.mesas, mesa)
+//			.innerJoin(mesa.reservas, reserva)
+//			.where(restaurante.id.eq(restauranteId)
+//			.and(mesa.capacidadePessoas.eq(capacidadePessoas) //terminou aq
+//			.and(reserva.dataReserva.eq(data))
+//			.and(reserva.status.eq())));
+//		
+//		return query.fetch();
+//	}
 }
